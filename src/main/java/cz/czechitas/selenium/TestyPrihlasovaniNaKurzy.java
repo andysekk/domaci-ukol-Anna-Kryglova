2c15437a5ac50b5860d88278b53e5940a997134d
Anna Kryglová <andysek.brno@seznam.cz>
2021-05-18 23:02:38 +0200

    doladění DÚ :o)

--- src/main/java/cz/czechitas/selenium/TestyPrihlasovaniNaKurzy.java
+++ src/main/java/cz/czechitas/selenium/TestyPrihlasovaniNaKurzy.java
@@ -12,35 +12,78 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class TestyPrihlasovaniNaKurzy<prihlaseniRodice> {
-
+    private static final String URL_APLIKACE = "https://cz-test-jedna.herokuapp.com/";
     WebDriver prohlizec;
-
-
 
     @BeforeEach
     public void setUp() {
         System.setProperty("webdriver.gecko.driver", "C:\\Java-Training\\Selenium\\geckodriver.exe");
         prohlizec = new FirefoxDriver();
+        prohlizec.navigate().to(URL_APLIKACE);
         prohlizec.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
-
+    @Test
+    public void prihlaseniRodiceDoSystemu() {
+        prihlaseniRodice();
+        overeniPrihlaseniRodice();
+    }
 
     @Test
     public void vyberKurzuPotePrihlaseni() {
-        prohlizec.navigate().to("https://cz-test-jedna.herokuapp.com/");
-        WebElement volbaTrimesicniKurzyProgramovani = prohlizec.findElement(By.xpath("( //div[@class = 'card'] )[3]//a[text()='Více informací']"));
-        volbaTrimesicniKurzyProgramovani.click();
-        WebElement volbaJavaScript = prohlizec.findElement(By.xpath("( //div[@class = 'card'] )[2]//a[text()='Vytvořit přihlášku']"));
-        volbaJavaScript.click();
+        vyberKurzuTrimesicniKurzyProgramovaniJavaskript();
+        prihlaseniRodiceDoSystemu();
+        vyplneniPrihlaskyTrimesicniKurzyProgramovaniJavaskript();
+        overeniPrihlaseniZakaDoKurzu();
+    }
 
+    @Test
+    public void prihlaseniPoteVyberKurzu() {
+        prihlaseniRodiceDoSystemu();
+        vyberKurzuDATestovaniZakladyAlgoritmizace();
+        vyplneniPrihlaskyKurzuDATestovaniZakladyAlgoritmizace();
+        overeniPrihlaseniZakaDoKurzu();
+    }
+
+    @Test
+    public void odhlaseniUcastiZakaZKurzu() {
+        prihlaseniRodiceDoSystemu();
+        vyberKurzuDATestovaniZakladyAlgoritmizace();
+        vyplneniPrihlaskyKurzuDATestovaniZakladyAlgoritmizace();
+        odhlaseniUcastiZaka();
+        overeniOdhlaseniZaka();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        prohlizec.close();
+    }
+
+
+    public void prihlaseniRodice() {
+        WebElement tlacitkoPrihlasit = prohlizec.findElement(By.xpath("//a[@href='https://cz-test-jedna.herokuapp.com/prihlaseni']"));
+        tlacitkoPrihlasit.click();
         WebElement prihlaseniEmail = prohlizec.findElement(By.id("email"));
         prihlaseniEmail.sendKeys("andysek.brno@seznam.cz");
         WebElement polickoHeslo = prohlizec.findElement(By.id("password"));
         polickoHeslo.sendKeys("Franta1");
-        WebElement tlacitkoPrihlasit = prohlizec.findElement(By.xpath("//button[contains(text(),'Přihlásit')]"));
-        tlacitkoPrihlasit.click();
+        WebElement tlacitkoPrihlasit2 = prohlizec.findElement(By.xpath("//button[contains(text(),'Přihlásit')]"));
+        tlacitkoPrihlasit2.click();
+    }
 
+    public void overeniPrihlaseniRodice() {
+        WebElement potvrzeniOPrihlaseniRodice = prohlizec.findElement(By.xpath("//nav/div/div[2]/div/a"));
+        Assertions.assertEquals("Franta Kocourek", potvrzeniOPrihlaseniRodice.getText());
+    }
+
+    public void vyberKurzuTrimesicniKurzyProgramovaniJavaskript() {
+        WebElement volbaTrimesicniKurzyProgramovani = prohlizec.findElement(By.xpath("( //div[@class = 'card'] )[3]//a[text()='Více informací']"));
+        volbaTrimesicniKurzyProgramovani.click();
+        WebElement volbaJavaScript = prohlizec.findElement(By.xpath("( //div[@class = 'card'] )[2]//a[text()='Vytvořit přihlášku']"));
+        volbaJavaScript.click();
+    }
+
+    public void vyplneniPrihlaskyTrimesicniKurzyProgramovaniJavaskript() {
         WebElement vyberTerminu = prohlizec.findElement(By.xpath("//*[contains(text(),'Vyberte termín...')]"));
         vyberTerminu.click();
         WebElement vyplneniTerminu = prohlizec.findElement(By.xpath("//*[contains(@class,'bs-searchbox')]//input"));
@@ -59,31 +102,23 @@ public class TestyPrihlasovaniNaKurzy<prihlaseniRodice> {
         tlacitkoSouhlasSPodminkami.click();
         WebElement tlacitkoVytvoritPrihlasku = prohlizec.findElement(By.xpath("//*[contains(@class,'btn btn-primary mt-3')]"));
         tlacitkoVytvoritPrihlasku.click();
-
-        WebElement potvrzeniOPrihlaseni = prohlizec.findElement(By.xpath("//*[contains(text(),'Stáhnout potvrzení o přihlášení')]"));
-        Assertions.assertNotNull(potvrzeniOPrihlaseni);
     }
 
-    @Test
-    public void prihlaseniPoteVyberKurzu() {
-        prohlizec.navigate().to("https://cz-test-jedna.herokuapp.com/");
-        WebElement tlacitkoPrihlasit = prohlizec.findElement(By.xpath("//a[@href='https://cz-test-jedna.herokuapp.com/prihlaseni']"));
-        tlacitkoPrihlasit.click();
-        WebElement prihlaseniEmail = prohlizec.findElement(By.id("email"));
-        prihlaseniEmail.sendKeys("andysek.brno@seznam.cz");
-        WebElement polickoHeslo = prohlizec.findElement(By.id("password"));
-        polickoHeslo.sendKeys("Franta1");
-        WebElement tlacitkoPrihlasit2 = prohlizec.findElement(By.xpath("//button[contains(text(),'Přihlásit')]"));
-        tlacitkoPrihlasit2.click();
+    public void overeniPrihlaseniZakaDoKurzu() {
+        WebElement potvrzeniOPrihlaseniZaka = prohlizec.findElement(By.xpath("//*[contains(@class,'mb-1 btn btn-sm btn-success')]"));
+        Assertions.assertEquals("Stáhnout potvrzení o přihlášení", potvrzeniOPrihlaseniZaka.getText());
+    }
 
+    public void vyberKurzuDATestovaniZakladyAlgoritmizace() {
         WebElement tlacitkoVytvoritNovouPrihlasku = prohlizec.findElement(By.xpath("//*[contains(@class,'btn btn-sm btn-info')]"));
         tlacitkoVytvoritNovouPrihlasku.click();
         WebElement volbaDigitalniAkademieTestovani = prohlizec.findElement(By.xpath("( //div[@class = 'card'] )[1]//a[text()='Více informací']"));
         volbaDigitalniAkademieTestovani.click();
         WebElement volbaZakladyAlgoritmizace = prohlizec.findElement(By.xpath("( //div[@class = 'card'] )[1]//a[text()='Vytvořit přihlášku']"));
         volbaZakladyAlgoritmizace.click();
+    }
 
-
+    public void vyplneniPrihlaskyKurzuDATestovaniZakladyAlgoritmizace() {
         WebElement vyberTerminu = prohlizec.findElement(By.xpath("//*[contains(text(),'Vyberte termín...')]"));
         vyberTerminu.click();
         WebElement vyplneniTerminu = prohlizec.findElement(By.xpath("//*[contains(@class,'bs-searchbox')]//input"));
@@ -102,45 +137,21 @@ public class TestyPrihlasovaniNaKurzy<prihlaseniRodice> {
         tlacitkoSouhlasSPodminkami.click();
         WebElement tlacitkoVytvoritPrihlasku = prohlizec.findElement(By.xpath("//*[contains(@class,'btn btn-primary mt-3')]"));
         tlacitkoVytvoritPrihlasku.click();
-
-        WebElement potvrzeniOPrihlaseni = prohlizec.findElement(By.xpath("//*[contains(text(),'Stáhnout potvrzení o přihlášení')]"));
-        Assertions.assertNotNull(potvrzeniOPrihlaseni);
-
     }
 
-    @Test
-    public void odhlaseniUcastiZKurzu() {
-
-        prohlizec.navigate().to("https://cz-test-jedna.herokuapp.com/");
-        WebElement tlacitkoPrihlasit = prohlizec.findElement(By.xpath("//a[@href='https://cz-test-jedna.herokuapp.com/prihlaseni']"));
-        tlacitkoPrihlasit.click();
-        WebElement prihlaseniEmail = prohlizec.findElement(By.id("email"));
-        prihlaseniEmail.sendKeys("andysek.brno@seznam.cz");
-        WebElement polickoHeslo = prohlizec.findElement(By.id("password"));
-        polickoHeslo.sendKeys("Franta1");
-        WebElement tlacitkoPrihlasit2 = prohlizec.findElement(By.xpath("//button[contains(text(),'Přihlásit')]"));
-        tlacitkoPrihlasit2.click();
-
-        WebElement tlacitkoOdhlaseniUcasti = prohlizec.findElement(By.xpath("(//div[@class='btn-group'])[3]//a[text()='Odhlášení účasti']"));
+    public void odhlaseniUcastiZaka() {
+        WebElement tlacitkoOdhlaseniUcasti = prohlizec.findElement(By.xpath("(//div[@class='btn-group'])[13]//a[text()='Odhlášení účasti']"));
         tlacitkoOdhlaseniUcasti.click();
         WebElement duvodOdhlaseniNemoc = prohlizec.findElement(By.xpath("//*[text()='Nemoc']"));
         duvodOdhlaseniNemoc.click();
         WebElement tlacitkoOdhlasitZaka = prohlizec.findElement(By.xpath("//*[@class='btn btn-primary']"));
         tlacitkoOdhlasitZaka.click();
-        WebElement tlacitkoDetail = prohlizec.findElement(By.xpath("( //div[@class = 'btn-group'] )[3]//a[text()='Detail']"));
+        WebElement tlacitkoDetail = prohlizec.findElement(By.xpath("( //div[@class = 'btn-group'] )[13]//a[text()='Detail']"));
         tlacitkoDetail.click();
-
-        WebElement potvrzeniOOdhlaseni = prohlizec.findElement(By.xpath("//*[contains(text(),'Žák byl odhlášen')]"));
-        Assertions.assertNotNull(potvrzeniOOdhlaseni);
-   }
-
-
-
-    @AfterEach
-    public void tearDown() {
-        prohlizec.close();
     }
 
-
-
+    public void overeniOdhlaseniZaka() {
+        WebElement potvrzeniOOdhlaseni = prohlizec.findElement(By.xpath("//li"));
+        Assertions.assertTrue(potvrzeniOOdhlaseni.getText().startsWith("Žák byl odhlášen"));
+    }
 }
